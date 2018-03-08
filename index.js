ffbc546f81c515d977c2a09ff5a1bc3468e69d98
Pierre-Yves Gérardy <pygy79@gmail.com>
2018-03-08 23:50:14 +0100

    Make it harder to spoof the detection 😘
    Just in case they wanted to pull another `document.all`...

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-if (!Array.prototype.smoosh) {
+try {[].smoosh()} catch(_) {
   Array.prototype.smoosh = function smoosh() {
     return this[0];
   };
 }
 
-if (!Array.prototype.smooshMap) {
+try {[].smooshMap(function(){})} catch(_) {
   Array.prototype.smooshMap = function smooshMap(fn) {
     return this.length > 0 ? fn(this[0]) : undefined;
   };
