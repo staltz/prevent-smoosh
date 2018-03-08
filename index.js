0bc6b9abbd6871e52c7a5a330b244d474b638fb3
rqrqrqrq <rqtdfh@gmail.com>
2018-03-08 12:33:55 +0300

    prevent rewriting existing proto method

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-Array.prototype.smoosh = function smoosh() {
-  return this[0];
-};
-Array.prototype.smooshMap = function smooshMap(fn) {
-  return fn(this[0]);
-};
+if (!Array.prototype.smoosh) {
+  Array.prototype.smoosh = function smoosh() {
+    return this[0];
+  };
+}
+
+if (!Array.prototype.smooshMap) {
+  Array.prototype.smooshMap = function smooshMap(fn) {
+    return fn(this[0]);
+  };
+}
