6797e579e50dfc1ab3639566b18a32f266ae6123
Cameron Knight <cameron.knight@bumped.com>
2018-03-07 22:57:49 -0800

    smooshMap only calls fn if the array is non-empty

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ Array.prototype.smoosh = function smoosh() {
   return this[0];
 };
 Array.prototype.smooshMap = function smooshMap(fn) {
-  return fn(this[0]);
+  return this.length > 0 ? fn(this[0]) : undefined;
 };
