575625475df78599c8cf74b1b0087d81f0191c7b
Mike <mike.grinspan@gmail.com>
2018-05-24 15:29:40 -0400

    You kiss your mother with that indentation?

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-try {[].smoosh()} catch(_) {
+try {
+  [].smoosh();
+} catch(_) {
   Array.prototype.smoosh = function smoosh() {
     return this[0];
   };
 }
 
-try {[].smooshMap(function(){})} catch(_) {
-  Array.prototype.smooshMap = function smooshMap(fn) {
-    return this.length > 0 ? fn(this[0]) : undefined;
+try {
+  [].smooshMap(function () {});
+} catch(_) {
+  Array.prototype.smooshMap = function smooshMap(callback) {
+    return this.length > 0 ? callback(this[0]) : undefined;
   };
 }
